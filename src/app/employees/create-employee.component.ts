08a5b1ef9c19c56eceb09b9458521ec1aa336f55
Syed Irfan <sirfan@teisd.com>
2019-08-05 17:23:18 -0500

    Part 10 - Angular populating drop-down list

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/departement.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -15,6 +16,14 @@ export class CreateEmployeeComponent implements OnInit {
 
   // default value for ddl
   department = 3;
+
+  departments: Department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' },
+    { id: 5, name: 'Admin' }
+  ];
 
   constructor() {}
 
